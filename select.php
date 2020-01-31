b6bb1b6156cb32e5f5d98a836cdf12a0b6c00831
Tekkun-0116 <tekkun.2358@gmail.com>
2020-02-01 00:53:19 +0900

    first commit

--- select.php
+++ select.php
@@ -12,13 +12,47 @@ try {
   exit;
 }
 
-$sql = "select * from animals";
+$sql = "select * from animals where discription like '%おっとり%'";
 $stmt = $dbh->prepare($sql);
+$stmt->execute();
+
+$sql2 = "select * from animals";
+$stmt = $dbh->prepare($sql2);
 $stmt->execute();
 
 $animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+  $discription = $_GET['discription'];
+};
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>課題</title>
+</head>
+<body>
+  <h1>本日のご紹介ペット！</h1>
+  <p>
+    <form action="" method="get">
+      キーワード:
+      <input type="text" name="discription" placeholder="キーワードの入力">
+      <input type="submit" value="検索">
+    </form>
+  </p>
+</body>
+</html>
+
+<?php
+
 foreach ($animals as $animal)
 {
-  echo $animal['type'] .  'の' . $animal['classifcation'] . 'ちゃん' .' <br> ' . $animal['description'] . ' <br> ' . $animal['birthday'] . '生まれ' . ' <br> ' . '出身地' . $animal['birthplace'] . ' <hr> ';
-} 
+  echo $animal['type'] .  'の' . $animal['classifcation'] . 'ちゃん' .' <br> ' . $animal['discription'] . ' <br> ' . $animal['birthday'] . '生まれ' . ' <br> ' . '出身地' . $animal['birthplace'] . ' <hr> ';
+}
+
+?>
